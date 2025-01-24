655296b08c39123bf670548630d0ba11d498341f
AhXiang <xiangxiang0917@outlook.com>
2025-01-25 01:20:05 +0800

    Script for root
    To initial game content.

---
+++ Assets/Scripts/GameRoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRoot : MonoBehaviour
+{
+    public static GameRoot instance;
+    public StartWindow startWindow;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        instance = this;
+        gameStartWindow();
+    }
+
+    private void gameStartWindow() 
+    {
+        Transform canvas = transform.Find("Canvas");
+        for (int i = 0; i < canvas.childCount; i++) 
+        {
+            canvas.GetChild(i).gameObject.SetActive(false);
+        }
+        startWindow.gameObject.SetActive(true);
+    }
+}
