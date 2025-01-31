7aa0865fa1cdba60d6f4909de8783e9222c376bd
AhXiang <xiangxiang0917@outlook.com>
2025-01-31 22:50:17 +0800

    update GameWindow script to control the game flow

--- Assets/Scripts/Windows/GameWindow.cs
+++ Assets/Scripts/Windows/GameWindow.cs
@@ -2,29 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameWindow : MonoBehaviour
+public class GameWindow : WindowRoot
 {
     public GameObject player;
-    public Transform startPoint;
+    public GameObject[] levels;
 
-    private void gameStart() 
+    private int levelCount;
+
+    private void Start()
+    {
+        levelCount = 0;
+        gameStart();
+        loadLevel();
+    }
+
+    private void gameStart()
     {
         player.SetActive(true);
     }
 
-    private void gameOver()
+    private void delLevel()
+    {
+        Transform currentLevel = transform.Find("Level" + levelCount);
+        if (currentLevel != null)
+        {
+            Destroy(currentLevel.gameObject.gameObject);
+        }
+    }
+
+    public void nextLevel()
+    {
+        delLevel();
+        levelCount++;
+        loadLevel();
+    }
+
+    public void gameOver()
     {
         player.SetActive(false);
     }
 
-    private void loadLevel() 
+    private void loadLevel()
     {
-        
+        // Load the current level and set it as a child of GameWindow
+        GameObject level = Instantiate(levels[levelCount]);
+        level.name = "Level" + levelCount;
+        level.transform.SetParent(transform, false);
+
+        // Position the player at the StartPoint of the new level
+        Transform start = level.transform.Find("StartPoint");
+        if (start != null)
+        {
+            player.transform.localPosition = start.localPosition;
+        }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R)) 
+        if (Input.GetKeyDown(KeyCode.R))
         {
             respawn();
         }
@@ -33,6 +68,7 @@ public class GameWindow : MonoBehaviour
     private void respawn()
     {
         gameStart();
-        player.transform.localPosition = startPoint.localPosition;
+        delLevel();
+        loadLevel();
     }
 }
