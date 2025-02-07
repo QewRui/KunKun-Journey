60433baa9c91b74d1c11a2fe039fc76771c2fe5d
AhXiang <xiangxiang0917@outlook.com>
2025-02-07 17:22:29 +0800

    added knockbackController for knockback effect

---
+++ Assets/Scripts/Game/knockBackController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class knockBackController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player") 
+        {
+            collision.gameObject.GetComponent<playerController>().knockBackCounter = collision.gameObject.GetComponent<playerController>().knockBackDuration;
+            if (collision.transform.position.x <= transform.position.x) 
+            {
+                collision.gameObject.GetComponent<playerController>().knockBackFromRight = true;
+            }
+            if (collision.transform.position.x > transform.position.x) 
+            {
+                collision.gameObject.GetComponent<playerController>().knockBackFromRight = false;
+            }
+        }
+    }
+}
