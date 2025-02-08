6c634975d6b85a9ecc7ccf45d5ecd68722e04b56
AhXiang <xiangxiang0917@outlook.com>
2025-02-08 15:21:31 +0800

    added pauseMenu

--- Assets/Scripts/Windows/GameWindow.cs
+++ Assets/Scripts/Windows/GameWindow.cs
@@ -7,23 +7,39 @@ public class GameWindow : WindowRoot
     public GameObject player;
     public GameObject[] levels;
     public GameObject gameOverTip; 
+    public pauseMenuController pauseMenu;
 
-    private int levelCount;
+    private GameObject keyIconPlay;
+    private GameObject keyIconExit;
 
-    private void Start()
+    public int levelCount;
+    private bool isDead;
+    private bool isPlayerNearby;
+    private string objectTag;
+
+    public void Start()
     {
         levelCount = 0;
         gameStart();
         loadLevel();
+
+        // Find key icons by tag
+        keyIconPlay = GameObject.Find("f_button_play");
+        keyIconExit = GameObject.Find("f_button_exit");
+
+        // Hide key icons at start
+        if (keyIconPlay) keyIconPlay.SetActive(false);
+        if (keyIconExit) keyIconExit.SetActive(false);
     }
 
     private void gameStart()
     {
+        isDead = false;
         player.SetActive(true);
         gameOverTip.SetActive(false);
     }
 
-    private void delLevel()
+    public void delLevel()
     {
         Transform currentLevel = transform.Find("Level" + levelCount);
         if (currentLevel != null)
@@ -41,11 +57,13 @@ public class GameWindow : WindowRoot
 
     public void gameOver()
     {
+        isDead = true;
+        Time.timeScale = 0;
         player.SetActive(false);
         gameOverTip.SetActive(true);  // Show GameOver Tip
     }
 
-    private void loadLevel()
+    public void loadLevel()
     {
         // Load the current level and set it as a child of GameWindow
         GameObject level = Instantiate(levels[levelCount]);
@@ -62,16 +80,65 @@ public class GameWindow : WindowRoot
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (isDead && Input.GetKeyDown(KeyCode.R))
         {
             respawn();
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.gameObject.activeSelf)
+            {
+                pauseMenu.Resume();
+            }
+            else
+            {
+                pauseMenu.Pause();
+            }
+        }
+
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.F))
+        {
+            if (objectTag == "ExitGameSign")
+            {
+                Application.Quit();
+            }
+            else if (objectTag == "StartGameSign")
+            {
+                nextLevel();
+            }
+        }
     }
 
-    private void respawn()
+    public void respawn()
     {
         gameStart();
         delLevel();
         loadLevel();
+
+        // Reassign key icons
+        keyIconPlay = GameObject.Find("f_button_play");
+        keyIconExit = GameObject.Find("f_button_exit");
+
+        // Hide them again at restart
+        if (keyIconPlay) keyIconPlay.SetActive(false);
+        if (keyIconExit) keyIconExit.SetActive(false);
+
+        Time.timeScale = 1;
+    }
+
+    public void signInteraction(string signTag, bool isEntering)
+    {
+        isPlayerNearby = isEntering;
+        objectTag = signTag; // Store the tag for Update() handling
+
+        if (signTag == "StartGameSign")
+        {
+            keyIconPlay.SetActive(isEntering);
+        }
+        else if (signTag == "ExitGameSign")
+        {
+            keyIconExit.SetActive(isEntering);
+        }
     }
 }
