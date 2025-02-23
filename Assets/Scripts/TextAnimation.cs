aa35b9e53a753e784e05075eba5242833e32ba1b
QewRui <showale122@gmail.com>
2025-02-23 12:12:31 +0800

    adjust title screen

---
+++ Assets/Scripts/TextAnimation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAnimation : MonoBehaviour
+{
+    public Text text;
+
+    private void Start()
+    {
+        if (text == null)
+        {
+            text = GetComponent<Text>();
+        }
+
+        StartBlinking();
+    }
+
+    private void OnEnable()
+    {
+        StartBlinking();
+    }
+
+    private void OnDisable()
+    {
+        StopBlinking();
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+            yield return new WaitForSeconds(1.0f);
+
+            text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    void StartBlinking()
+    {
+        StopBlinking();
+        StartCoroutine(Blink());
+    }
+
+    void StopBlinking()
+    {
+        StopCoroutine(Blink());
+    }
+}
